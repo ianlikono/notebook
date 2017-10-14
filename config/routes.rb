84bf2995170549881893598d2c5e343b64cd130a
ianlikono <ianlikono@gmail.com>
2017-10-14 10:08:51 +0300

    root_based on if user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :models
   get 'welcome/index'
+  resources :notes
 
+  authenticated :user do
+  	root 'notes#index', as: "authenticated_root"
+  end
   root 'welcome#index'
 
-  resources :notes
+  
 
   
 end
